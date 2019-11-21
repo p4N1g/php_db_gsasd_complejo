0cbe7811fb3cb2c6301ee7d8ffaf85bb5b97d2ac
Fco Cordoba Rey <i72coref@uco.es>
2019-11-21 20:20:51 +0100

    Modificando acceso a la BD

--- conexion.php
+++ conexion.php
@@ -1,12 +1,17 @@
 <?php    
+    $dbhost = getenv("MYSQL_SERVICE_HOST");
+    $dbport = getenv("MYSQL_SERVICE_PORT");
+    $dbuser = getenv("databaseuser");
+    $dbpwd = getenv("databasepassword");
+    $dbname = getenv("databasename");
     // conectar al servidor de BD
-    $conex = mysql_connect("localhost","root","");
+    $conex = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
     // controlar conexión
     if (!$conex) {
         header("Location: errorPage.php?MSG=NO se pudo CONECTAR al SERVIDOR de Base de Datos");
     } // endif
     // seleccionar Base de Datos
-    $selDB = mysql_select_db("webmarket",$conex);
+    $selDB = mysql_select_db($dbname,$conex);
     // controlar selección de Base de Datos
     if (!$selDB) {
         header("Location: errorPage.php?MSG=NO se pudo SELECCIONAR la Base de Datos");
